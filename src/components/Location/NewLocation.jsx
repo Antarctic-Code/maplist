1d1c589b90e530e02ad6fea83caf91a3d27e4941
Jorge Alviarez <jjafblin@gmail.com>
2021-06-27 19:18:22 -0500

    add map insie from

--- src/components/Location/NewLocation.jsx
+++ src/components/Location/NewLocation.jsx
@@ -1,8 +1,14 @@
-import React, { useState, /*useContext,*/ Fragment } from "react";
+import React, { useState, useEffect,useMemo,useRef } from "react";
 import clienteAxios from "../../config/axios";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import Swal from "sweetalert2";
 import { withRouter } from "react-router-dom";
 //Importar el context
+
+const mapStyle = {
+  height: '40vh',
+  width: '60vh'
+};
 
 const NewLocation = ({ history }) => {
   //Trabajar con el state
@@ -10,8 +16,8 @@ const NewLocation = ({ history }) => {
   const [location, gurardarLocation] = useState({
     titulo: "",
     direccion: "",
-    lat: 0,
-    lng: 0,
+    lat: 9.004575579774295,
+    lng: -79.52108424083316,
     userId: 1,
   });
 
@@ -46,6 +52,25 @@ const NewLocation = ({ history }) => {
       });
   };
 
+  const markerRef = useRef(null)
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current
+        if (marker != null) {
+          const newmarket = marker.getLatLng();
+          console.log(newmarket)
+          gurardarLocation({
+            ...location,
+            lat: newmarket.lat,
+            lng: newmarket.lng,
+          });
+        }
+      },
+    }),
+    [],
+  )
+
   const handleChange = (e) => {
     gurardarLocation({
       //Actualuzamos el state agregando una copia del State actual
@@ -62,7 +87,7 @@ const NewLocation = ({ history }) => {
   };
 
   return (
-    <Fragment>
+    <>
       <h2>Guardar Lugar</h2>
       <form onSubmit={handleSubmit}>
         <legend>Llena todos los campos</legend>
@@ -84,6 +109,25 @@ const NewLocation = ({ history }) => {
             onChange={handleChange}
           />
         </div>
+        <div className="campo">          
+          <MapContainer center={[location.lat, location.lng]} zoom={19} style={mapStyle} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker 
+              position={[location.lat, location.lng]}
+              draggable
+              eventHandlers={eventHandlers}
+              ref={markerRef}
+            >
+              <Popup>
+                <h1>{location.titulo}</h1>
+                <p>{location.lat},{location.lng}</p>
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
         <div className="campo">
           <label>Latitud:</label>
           <input
@@ -91,6 +135,7 @@ const NewLocation = ({ history }) => {
             placeholder="Latitud"
             name="lat"
             onChange={handleChange}
+            value={location.lat}
           />
         </div>
         <div className="campo">
@@ -100,6 +145,7 @@ const NewLocation = ({ history }) => {
             placeholder="Longitud"
             name="lng"
             onChange={handleChange}
+            value={location.lng}
           />
         </div>
         <div className="enviar">
@@ -111,7 +157,7 @@ const NewLocation = ({ history }) => {
           />
         </div>
       </form>
-    </Fragment>
+    </>
   );
 };
 
